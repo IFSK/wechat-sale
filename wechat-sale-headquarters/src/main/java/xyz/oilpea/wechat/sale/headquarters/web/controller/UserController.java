b6b942445d659b137755ce5fb0b3bb6e666f3152
StoryKing123 <34782804+StoryKing123@users.noreply.github.com>
2018-08-20 15:22:34 +0800

    模板

---
+++ wechat-sale-headquarters/src/main/java/xyz/oilpea/wechat/sale/headquarters/web/controller/UserController.java
@@ -0,0 +1,21 @@
+package xya.oilpea.wechat.sale.headquarters.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import xyz.oilpea.wechat.sale.core.User;
+
+
+@RestController
+@RequestMapping("/hello")
+public class UserController {
+	@Autowired
+	User user;
+
+	@GetMapping
+	public void showSpring() {
+		user.sayHello();
+	}
+}
