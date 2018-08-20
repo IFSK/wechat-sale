d107f9e83b737080a8a0af42fd1b36c793a57127
StoryKing123 <34782804+StoryKing123@users.noreply.github.com>
2018-08-20 15:25:17 +0800

    模板

---
+++ wechat-sale/wechat-sale-core/src/main/java/xyz/oilpea/wechat/sale/core/User.java
@@ -0,0 +1,11 @@
+package xyz.oilpea.wechat.sale.core;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class User {
+
+	public void sayHello() {
+		System.out.println("hello");
+	}
+}
---
+++ wechat-sale/wechat-sale-headquarters/src/main/java/xyz/oilpea/wechat/sale/headquarters/HeadQuartersApplication.java
@@ -0,0 +1,15 @@
+package xyz.oilpea.wechat.sale.headquarters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = { "xyz.oilpea.wechat.sale" })
+@ComponentScan(basePackages = { "xyz.oilpea.*" })
+@SpringBootApplication
+public class HeadQuartersApplication {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(HeadQuartersApplication.class, args);
+	}
+
+}
