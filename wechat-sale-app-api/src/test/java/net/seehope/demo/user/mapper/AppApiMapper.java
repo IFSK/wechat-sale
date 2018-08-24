ce0b0d9a543c849205f00277eba52d9a1faf6b41
IllusoryFroth <345006433@qq.com>
2018-08-24 23:24:39 +0800

    删了些弄错了的逆向工程

--- wechat-sale-app-api/src/test/java/net/seehope/demo/user/mapper/AppApiMapper.java
+++
@@ -1,7 +0,0 @@
-package net.seehope.demo.user.mapper;
-
-import net.seehope.demo.user.pojo.AppApi;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface AppApiMapper extends Mapper<AppApi> {
-}
