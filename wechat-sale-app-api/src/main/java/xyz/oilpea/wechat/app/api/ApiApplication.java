680f8aa6ebcc1af9bf4af2367180c58a2e9ccd5a
IllusoryFroth <345006433@qq.com>
2018-08-26 17:37:12 +0800

    test

--- wechat-sale-app-api/src/main/java/xyz/oilpea/wechat/app/api/ApiApplication.java
+++ wechat-sale-app-api/src/main/java/xyz/oilpea/wechat/app/api/ApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiApplication {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(ApiApplication.class, args);
+		System.out.println("123");
 	}
 
 }
