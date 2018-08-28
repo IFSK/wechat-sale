fbb11f232ed4a35c0ab13e9dd301a1d1fde9de7d
IllusoryFroth <345006433@qq.com>
2018-08-28 18:48:36 +0800

    test

--- wechat-sale-agency/src/main/java/xyz/oilpea/wechat/agency/web/controller/LoginController.java
+++ wechat-sale-agency/src/main/java/xyz/oilpea/wechat/agency/web/controller/LoginController.java
@@ -1,10 +1,10 @@
 package xyz.oilpea.wechat.agency.web.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("login")
+@RestController
+@RequestMapping("/login")
 public class LoginController {
 
 }
